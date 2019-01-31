db185f61ec3e529a37f9b06c1fc18f83e609bc4b
Shubham-tiwari123 <st540362@gmail.com>
2019-01-31 21:49:32 +0530

    Searching algo file

--- SearchingAlgo.java
+++ SearchingAlgo.java
@@ -3,8 +3,75 @@ package algorithm;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class SearchingAlgo {
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        switch (n) {
+            case 1:
+                int arr1[] = {1, 12, 15, 26, 38};
+                int arr2[] = {2, 13, 17, 30, 45};
+
+                int len1 = arr1.length;
+                int len2 = arr2.length;
+
+                if (len1 == len2) {
+                    medianOfArray(arr1, arr2, len1);
+                } else {
+                    System.out.println("Array of uneven length");
+                }
+                break;
+
+            case 2:
+                int arr[] = {12, 13, 1, 10, 34, 1};
+                secondSmallestNumber(arr);
+                break;
+
+            case 3:
+                int arr4[] = {1000, 11, 445, 1, 330, 3000};
+                minAndMax(arr4);
+                break;
+
+            case 4:
+                Integer arr5[] = {1, 23, 12, 9, 30, 2, 50};
+                int k = 3;
+                kLargestElement(arr5, k);
+                break;
+
+            case 5:
+                int arr6[] = {1, 1, 2, 2, 2, 2, 3};
+                n = 2;
+                numberOfOccurence(arr6, n);
+                break;
+
+            case 6:
+                Integer arr7[] = {4, 3, 6, 2, 1, 1};
+                findRepeatingAndMissing(arr7);
+                break;
+
+            case 7:
+                int arr8[] = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
+                n = arr8.length;
+                int ans = fixedPoint(arr8, 0, n - 1);
+                System.out.println("fixed point is:-" + ans);
+                break;
+
+            case 8:
+                int arr9[] = {1, 3, 50, 10, 9, 7, 6};
+                System.out.println("Maximum element:-"
+                        + findLargestElement(arr9, 0, (arr9.length - 1)));
+                break;
+
+            case 9:
+                int arr10[] = {1, 8, 30, 40, 100};
+                n = 60;
+                findDifference(arr10, n);
+                break;
+        }
+    }
 
     public void medianOfArray(int arr1[], int arr2[], int n) {
         int i = 0;
@@ -117,59 +184,65 @@ public class SearchingAlgo {
         }
         System.out.println(repeatedNum);
     }
-    
-    public int fixedPoint(int arr[],int start,int end){
-        if(end>=start){
-            int mid = (start+end)/2;
-            System.out.println("mid:-"+mid);
-            if(mid == arr[mid]){
-                System.out.println("mid2_"+mid);
+
+    public int fixedPoint(int arr[], int start, int end) {
+        if (end >= start) {
+            int mid = (start + end) / 2;
+            System.out.println("mid:-" + mid);
+            if (mid == arr[mid]) {
+                System.out.println("mid2_" + mid);
                 return mid;
             }
-            if(mid>arr[mid])
-                return fixedPoint(arr, (mid+1), end);
-            else
-                return fixedPoint(arr, start, (mid-1));
+            if (mid > arr[mid]) {
+                return fixedPoint(arr, (mid + 1), end);
+            } else {
+                return fixedPoint(arr, start, (mid - 1));
+            }
         }
         return -1;
-    } 
-    
-    public int findLargestElement(int arr[],int start,int end){
-        
-        if(start==end)
-            return arr[start];
-        
-        if((start == end+1) && arr[start]>=arr[end])
-            return arr[start];
-        
-        if((start == end+1) && arr[start]<arr[end])
-            return arr[end];
-        
-        int mid = (start+end)/2;
-        
-        if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
-            return arr[mid];
-        
-        if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) 
-            return findLargestElement(arr, start, mid-1); 
-        else 
-            return findLargestElement(arr, mid + 1, end); 
     }
-    
-    public int findDifference(int arr[],int n){
-        int size = arr.length; 
-        int i = 0, j = 1; 
-        while (i < size && j < size){ 
-            if (i != j && arr[j]-arr[i] == n){ 
-                System.out.print("Found:("+arr[i]+","+ arr[j]+")"); 
-                return 0; 
-            } 
-            else if (arr[j] - arr[i] < n) 
-                j++; 
-            else
-                i++; 
-        } 
-        System.out.print("No such pair"); 
-        return -1; 
+
+    public int findLargestElement(int arr[], int start, int end) {
+
+        if (start == end) {
+            return arr[start];
+        }
+
+        if ((start == end + 1) && arr[start] >= arr[end]) {
+            return arr[start];
+        }
+
+        if ((start == end + 1) && arr[start] < arr[end]) {
+            return arr[end];
+        }
+
+        int mid = (start + end) / 2;
+
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+            return arr[mid];
+        }
+
+        if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) {
+            return findLargestElement(arr, start, mid - 1);
+        } else {
+            return findLargestElement(arr, mid + 1, end);
+        }
+    }
+
+    public int findDifference(int arr[], int n) {
+        int size = arr.length;
+        int i = 0, j = 1;
+        while (i < size && j < size) {
+            if (i != j && arr[j] - arr[i] == n) {
+                System.out.print("Found:(" + arr[i] + "," + arr[j] + ")");
+                return 0;
+            } else if (arr[j] - arr[i] < n) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        System.out.print("No such pair");
+        return -1;
     }
 }
